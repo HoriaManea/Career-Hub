9b4ddec3ec41ce86acbd0527fd9b17ed5093c95c
Horia Manea <horiamanea.m@gmail.com>
2026-08-15 16:39:32 +0300

    feat(): create all pages

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -1,17 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 export default function Navigation() {
   return (
     <nav className="flex flex-row justify-between  pt-5">
-      <h1 className=" cursor-pointer text-2xl font-bold tracking-tight text-[var(--text-light)]">
-        Career<span className="text-[var(--light-green)]">-Hub</span>
-      </h1>
+      <NavLink to="/">
+        <h1 className=" cursor-pointer text-2xl font-bold tracking-tight text-[var(--text-light)]">
+          Career<span className="text-[var(--light-green)]">-Hub</span>
+        </h1>
+      </NavLink>
+
       <ul className="flex flex-row gap-6 items-center">
-        <li className="text-[var(--text-light)] cursor-pointer">Home</li>
-        <li className="text-[var(--text-light)] cursor-pointer">Jobs</li>
-        <li className="text-[var(--text-light)] cursor-pointer">Explore</li>
-        <li className="text-[var(--text-light)] cursor-pointer">Category</li>
-        <li className="text-[var(--text-light)] cursor-pointer">Pages</li>
+        <NavLink to="/">
+          <li className="text-[var(--text-light)] cursor-pointer">Home</li>
+        </NavLink>
+        <NavLink to="/jobs">
+          <li className="text-[var(--text-light)] cursor-pointer">Jobs</li>
+        </NavLink>
+        <NavLink to="/explore">
+          <li className="text-[var(--text-light)] cursor-pointer">Explore</li>
+        </NavLink>
+        <NavLink to="/category">
+          <li className="text-[var(--text-light)] cursor-pointer">Category</li>
+        </NavLink>
+        <NavLink to="/pages">
+          <li className="text-[var(--text-light)] cursor-pointer">Pages</li>
+        </NavLink>
       </ul>
       <div className="flex gap-6">
         <button className="hover:-translate-y-[2px] active:opacity-70 hover:shadow-[0_6px_20px_rgba(0,255,140,0.25)] transition-all duration-200 ease-out rounded-full cursor-pointer text-[var(--text-light)] border-solid border-2 pr-6 pl-6 pt-1 pb-1">
