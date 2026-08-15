259dc10ecbf8eeefbddaf6562139e2d77e998d95
Horia Manea <horiamanea.m@gmail.com>
2026-08-15 16:07:37 +0300

    feat(): create Navigation custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, type FormEvent } from "react";
 import { useDebounce } from "./hooks/useDebounce";
 import Input from "./components/layout/Input";
+import Navigation from "./components/ui/Navigation";
 
 type DataForm = {
   name: string;
@@ -21,28 +22,7 @@ function AppTwo() {
 
   console.log("Debounced:", debouncedForm);
 
-  return (
-    <>
-      <form onSubmit={handleSubmit} className="innerDiv">
-        <Input
-          value={form.name}
-          onChange={(value) => setForm((prev) => ({ ...prev, name: value }))}
-        />
-        <Input
-          value={form.password}
-          onChange={(value) =>
-            setForm((prev) => ({ ...prev, password: value }))
-          }
-          type="password"
-        />
-        <button type="submit" className="formInput">
-          Submit
-        </button>
-      </form>
-      <h1>{debouncedForm.name}</h1>
-      <h1>{debouncedForm.password}</h1>
-    </>
-  );
+  return <Navigation />;
 }
 
 export default AppTwo;
