9c538b5f0afca7137a35d8daca87045189daa152
Horia Manea <horiamanea.m@gmail.com>
2026-08-20 01:32:13 +0300

    feat(): create tanstack query client

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { SettingsProvider } from "./contexts/SettingsContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ShowModalProvider } from "./contexts/ShowModalContext";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
+    <QueryClientProvider client={queryClient}>
+      <SettingsProvider>
+        <ShowModalProvider>
+          <App />
+        </ShowModalProvider>
+      </SettingsProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
